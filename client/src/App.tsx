cfbfd43f47bea2a05c743755e32ce1916c9ff3b3
Dz.dev <darwinbjordan@gmail.com>
2024-06-07 22:30:41 +0800

    chore: added react router for Notes and Create Notes added it to Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,40 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Note from "./components/Note";
 import Notepad from "./components/Notepad";
+
+const queryClient = new QueryClient();
+
 export default function App() {
-  return (
-    <div>
-      <Header />
-      <Notepad />
-      <Note />
-      <Footer />
-    </div>
-  );
+  function Layout() {
+    return (
+      <div>
+        <Header />
+        <QueryClientProvider client={queryClient}>
+          <Outlet />
+        </QueryClientProvider>
+        <Footer />
+      </div>
+    );
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "notes",
+          element: <Note />,
+        },
+        {
+          path: "create",
+          element: <Notepad />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,21 @@
-import DzLogo from "../../public/image/DzLogo.png";
+import { Link } from "react-router-dom";
+import DzLogo from "/image/DzLogo.png";
 
 export default function Header() {
   return (
-    <header className="px-3 py-1 flex items-center bg-zinc-900 w-[100%] h-20">
+    <header className="px-3 py-1 flex items-center  bg-zinc-900 w-[100%] h-20">
       <h1 className="font-bold text-[2rem] text-[#dcdcdc]">Dz Pen</h1>
       <img src={DzLogo} alt="" className="w-10" />
+      <nav className="ml-auto pr-8 font-bold text-[1.5rem] text-[#dcdcdc]">
+        <ul className="flex gap-8">
+          <li>
+            <Link to="/notes">Notes</Link>
+          </li>
+          <li>
+            <Link to="/create">Create</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
